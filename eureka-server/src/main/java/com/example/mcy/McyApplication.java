e8c72524b98f50fdbffdccbe93cae8d936c6077e
mcy <machenyang@haitunsiwei.com>
2019-07-11 18:09:06 +0800

    first commit

---
+++ eureka-server/src/main/java/com/example/mcy/McyApplication.java
@@ -0,0 +1,13 @@
+package com.example.mcy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McyApplication.class, args);
+    }
+
+}
